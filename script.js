08cd6661512d24b46f1c31b5b135049c0a6c810a
Vadik <amerkhanov56@gmail.com>
2020-10-08 18:47:43 +0500

    mask

---
+++ script.js
@@ -0,0 +1,15 @@
+let ow;
+let oh;
+
+let facetrack;
+let videoInput;
+
+let imgMask;
+let imgFace;
+
+let selected = -1;
+
+function preload() {
+  imgMask = loadImage("mask2.png");
+  imgFace = loadImage("https://img.icons8.com/officel/2x/user.png");
+}
